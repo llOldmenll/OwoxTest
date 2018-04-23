1da6a813b26f9be13ab1b29d6a04738238cfb713
llOldmenll <nosovmak@gmail.com>
2018-04-23 08:32:27 +0300

    - crashlytics was added

--- app/src/main/java/com/oldmen/owoxtest/utils/CustomApplication.java
+++ app/src/main/java/com/oldmen/owoxtest/utils/CustomApplication.java
@@ -4,9 +4,11 @@ import android.app.Application;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.oldmen.owoxtest.data.database.AppDataBase;
 import com.oldmen.owoxtest.data.repositories.SharedPrefHelper;
 
+import io.fabric.sdk.android.Fabric;
 import java.lang.ref.WeakReference;
 
 import io.reactivex.Completable;
@@ -23,6 +25,7 @@ public class CustomApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         instance = this;
         dataBase = Room.databaseBuilder(getApplicationContext(), AppDataBase.class, DATA_BASE_NAME)
                 .build();
